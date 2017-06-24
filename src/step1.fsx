5372d04763a1ce4a891be167a0bedcaab9964b24
maestrow <andrew.shmelev@gmail.com>
2017-06-24 22:42:06 +0300

    save laptop work

--- src/step1.fsx
+++ src/step1.fsx
@@ -3,21 +3,16 @@ open System
 type Model = Task list
 and Task = { id: int; name: string; completedTime: DateTime option }
 
-type Action = 
-  | AddTask of string
-  | UpdateTask of int * string
-  | CompleteTask of int
-  | UnCompleteTask of int
-  | RemoveTask of int
-
-let renderModel (model: Model) = 
+module Views = 
   let renderTask (task: Task) = 
     let timeToStr = function 
       | None -> String.Empty
       | Some (value: DateTime) -> value.ToString(" -> dd.MM.yy hh:mm:ss")
     printfn "%d. %s%s" task.id task.name (timeToStr task.completedTime)
-  model
-  |> List.iter renderTask
+
+  let renderModel (model: Model) = 
+    model
+    |> List.iter renderTask
 
 let init = 
   [
@@ -26,4 +21,4 @@ let init =
      { id = 3; name = "333"; completedTime = None }
   ]
 
-init |> renderModel
+init |> Views.renderModel
--- src/step2.fsx
+++ src/step2.fsx
@@ -7,32 +7,38 @@ type Action =
   | AddTask of string
   | UpdateTask of int * string
   | CompleteTask of int
-  | UnCompleteTask of int
+  | UncheckTask of int
   | RemoveTask of int
 
-let renderModel (model: Model) = 
+module Views = 
   let renderTask (task: Task) = 
     let timeToStr = function 
       | None -> String.Empty
       | Some (value: DateTime) -> value.ToString(" -> dd.MM.yy hh:mm:ss")
     printfn "%d. %s%s" task.id task.name (timeToStr task.completedTime)
-  model
-  |> List.iter renderTask
 
+  let renderTasks (model: Task list) = 
+    model
+    |> List.iter renderTask
 
-let getAction 
+  let renderModel (model: Model) = 
+    model
+    |> List.iter renderTask
 
-let renderActions () = 
-  [
-    "1. AddTask"
-    "2. UpdateTask"
-    "3. CompleteTask"
-    "4. UnCompleteTask"
-    "5. RemoveTask"
-    "0. Quit"
-  ]
-  |> List.iter (printfn "%s")
-  Console.ReadLine()
+  let renderMainMenu () = 
+    [
+      "1. AddTask"
+      "2. UpdateTask"
+      "3. CompleteTask"
+      "4. UnCompleteTask"
+      "5. RemoveTask"
+      "0. Quit"
+    ]
+    |> List.iter (printfn "%s")
+    let input = Console.ReadLine()
+    match input with
+      | "0" -> 
+      | "1" -> 
 
 let view (model: Model) = 
   printfn "Your tasks:"
