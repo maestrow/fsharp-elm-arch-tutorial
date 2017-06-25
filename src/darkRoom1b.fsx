3353c31feda12799348f8d3ea00e301f26d335cc
User <User>
2017-06-25 13:06:08 +0300

    Introducing dark room: Simple version. StackOverflow issue challenge. Added docs file.

---
+++ src/darkRoom1b.fsx
@@ -0,0 +1,80 @@
+(*
+  Первый вариант реализации - Демонстрация StackOverflowException
+*)
+open System
+
+type Model = 
+  {
+    hero: Location
+    treasure: Location
+    roomSize: Size
+  }
+and Location = { x: int; y: int }
+and Size = { width: int; height: int }
+
+type Action = 
+  | North
+  | South
+  | West
+  | East
+
+let random = Random ()
+
+let update (model: Model) (action: Action) : Model = 
+  let fixValue (value: int) (max: int) = Math.Min (Math.Max(value, 1), max)
+  let (dx, dy) = 
+    match action with
+    | North -> (0, -1)
+    | South -> (0, 1)
+    | West -> (-1, 0)
+    | East -> (1, 0)
+  let newLocation = 
+    {
+      x = fixValue (model.hero.x + dx) model.roomSize.width
+      y = fixValue (model.hero.y + dy) model.roomSize.height
+    }
+  { model with hero = newLocation }
+
+let view (model: Model) (dispatcher: Action -> unit) = 
+  if model.hero = model.treasure then 
+    printfn "Congratulations! You found the treasure."
+  else
+    printf "Your location: (%d, %d). Where to move next? (N)orth, (S)outh, (W)est, (E)ast : " model.hero.x model.hero.y
+    //let choice = (Console.ReadLine()).ToUpper()
+    let choice = ["N"; "S"; "W"; "E"].[random.Next(4)]
+    printfn "%s" choice
+    let action = 
+      match choice with
+      | "N" -> Some North
+      | "S" -> Some South
+      | "W" -> Some West
+      | "E" -> Some East
+      | _ -> None
+    if action.IsNone then
+      printfn "Incorrect direction. Valid inputs are: N, S, W, E. Try again."
+    else
+      dispatcher action.Value
+      
+let init () = 
+  let (width, height) = (4, 4)
+  let getRandomLocation () = 
+    { x = random.Next(1, width+1); y = random.Next (1, height+1) }
+  {
+    hero = getRandomLocation ()
+    treasure = { x = -1; y = -1 } //getRandomLocation ()
+    roomSize = { width = width; height = height }
+  }
+
+let runApp (init: unit -> Model) (update: Model -> Action -> Model) (view: Model -> (Action -> unit) -> unit) = 
+  let mutable model = init ()
+  
+  let dispatcher action = 
+    model <- update model action
+
+  let rec loop () : unit = 
+    view model dispatcher
+    loop ()
+  loop ()
+  
+
+runApp init update view
--- src/step2.fsx
+++ src/step2.fsx
@@ -1,56 +1,101 @@
+(*
+  Пока приложение открыто, пользователь всегда может выполнить некоторое действие.
+  В консольном варианте приложения это означает, что после любого вывода данных ожидается ввод.
+*)
+
 open System
 
-type Model = Task list
-and Task = { id: int; name: string; completedTime: DateTime option }
+type Model = 
+  { 
+    tasks: Task list
+    display: Display
+  }
+and Task = { id: int; name: string }
+and Display = 
+  | AddTask
+  | RemoveTask
+  | Tasks
+  | MainMenu
+  | Error of string
 
 type Action = 
   | AddTask of string
-  | UpdateTask of int * string
-  | CompleteTask of int
-  | UncheckTask of int
   | RemoveTask of int
 
-module Views = 
-  let renderTask (task: Task) = 
-    let timeToStr = function 
-      | None -> String.Empty
-      | Some (value: DateTime) -> value.ToString(" -> dd.MM.yy hh:mm:ss")
-    printfn "%d. %s%s" task.id task.name (timeToStr task.completedTime)
+module Helpers = 
+  let mutable private identity = 0
+  
+  let private incrementId () = 
+    identity <- identity + 1
+    identity
 
-  let renderTasks (model: Task list) = 
-    model
-    |> List.iter renderTask
+  let createTask name = 
+    { id = incrementId (); name = name }
 
-  let renderModel (model: Model) = 
-    model
-    |> List.iter renderTask
+let update (model: Model) (action: Action) : Model = 
+  match action with
+    | Action.AddTask taskName ->
+      let tasks = (Helpers.createTask taskName) :: model.tasks
+      { tasks = tasks; display = Display.Tasks }
+    | Action.RemoveTask id -> 
+      let tasks = model.tasks |> List.filter (fun t -> t.id <> id)
+      { tasks = tasks; display = Display.Tasks }
 
-  let renderMainMenu () = 
+module Render = 
+  
+  let rec showTasks (model: Model) = 
+    printfn "Your tasks:"
+    model.tasks |> List.iter (fun task -> printfn "%d. %s" task.id task.name)
+    main { tasks = model.tasks; display = Display.MainMenu }
+  
+  and mainMenu (model: Model) =
+    printfn "Choose action:"
     [
       "1. AddTask"
-      "2. UpdateTask"
-      "3. CompleteTask"
-      "4. UnCompleteTask"
-      "5. RemoveTask"
+      "2. RemoveTask"
       "0. Quit"
     ]
     |> List.iter (printfn "%s")
-    let input = Console.ReadLine()
-    match input with
-      | "0" -> 
-      | "1" -> 
+    let userChoice = Console.ReadLine ()
+    let display = match userChoice with
+      | "0" -> Environment.Exit(0); Display.Tasks
+      | "1" -> Display.AddTask
+      | "2" -> Display.RemoveTask
+      | _ -> Display.Error "Incorrect input. Try again."
+    main { tasks = model.tasks; display = display }
 
-let view (model: Model) = 
-  printfn "Your tasks:"
-  printfn "Choose action:"
+  and showError message (model: Model) = 
+    printfn "%s" message
+    mainMenu model
+
+  and addTask (model: Model) = 
+    printf "Enter task name: "
+    let taskName = Console.ReadLine ()
+    let newModel = update model (Action.AddTask taskName)
+    main newModel
+
+  and removeTask (model: Model) = 
+    printf "Enter task id to remove: "
+    let taskId = Console.ReadLine ()
+    let newModel = update model (Action.RemoveTask (int taskId))
+    main newModel
+
+  and main (model: Model) = 
+    match model.display with
+      | Display.ShowTasks -> tasks ()
+      | Display.AddTask -> addTask model
+      | Display.RemoveTask -> removeTask model
+
 
 
 let init = 
-  [
-     { id = 1; name = "111"; completedTime = None }
-     { id = 2; name = "222"; completedTime = Some DateTime.Now }
-     { id = 3; name = "333"; completedTime = None }
-  ]
+  let tasks = 
+    [
+       { id = 1; name = "111" }
+       { id = 2; name = "222" }
+       { id = 3; name = "333" }
+    ]
+  { tasks = tasks; display = Display.ShowTasks }
 
 let update (model: Model) (action: Action) : Model =
   init
